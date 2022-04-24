a991abc9d8e6fbd578596683bf08a1c991a47e64
Schmid7k <schmid7k@protonmail.com>
2022-04-24 13:24:07 +0200

    Optimization update

--- src/aes_gcm.rs
+++ src/aes_gcm.rs
@@ -17,11 +17,11 @@ fn bench(c: &mut Criterion<CyclesPerByte>) {
         group.throughput(Throughput::Bytes(*size as u64));
 
         group.bench_function(BenchmarkId::new("encrypt-128", size), |b| {
-            b.iter(|| cipher128.encrypt(&nonce, &*buf));
+            b.iter(|| cipher128.encrypt(&nonce, &*buf).unwrap());
         });
 
         group.bench_function(BenchmarkId::new("encrypt-256", size), |b| {
-            b.iter(|| cipher256.encrypt(&nonce, &*buf));
+            b.iter(|| cipher256.encrypt(&nonce, &*buf).unwrap());
         });
     }
 
@@ -35,3 +35,49 @@ criterion_group!(
 );
 
 criterion_main!(benches);
+
+/*
+use aes_gcm::aead::heapless::Vec;
+use aes_gcm::aead::{AeadInPlace, NewAead};
+use aes_gcm::{Aes128Gcm, Aes256Gcm};
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
+use criterion_cycles_per_byte::CyclesPerByte;
+use rust_crypto_aes_benchmarks::KB;
+
+fn bench(c: &mut Criterion<CyclesPerByte>) {
+    let mut group = c.benchmark_group("aes-gcm");
+
+    let cipher128 = Aes128Gcm::new(&Default::default());
+    let cipher256 = Aes256Gcm::new(&Default::default());
+    let nonce = &Default::default();
+
+    for size in &[KB, 2 * KB, 4 * KB, 8 * KB, 16 * KB] {
+        let mut buf: Vec<u8, 1040> = Vec::new();
+
+        let ext = vec![0u8; *size];
+
+        buf.extend(ext.into_iter());
+
+        group.throughput(Throughput::Bytes(*size as u64));
+
+        group.bench_function(BenchmarkId::new("encrypt-128", size), |b| {
+            b.iter(|| cipher128.encrypt_in_place(&nonce, b"", &mut buf));
+        });
+
+        group.bench_function(BenchmarkId::new("encrypt-256", size), |b| {
+            b.iter(|| cipher256.encrypt_in_place(&nonce, b"", &mut buf));
+        });
+    }
+
+    group.finish();
+}
+
+criterion_group!(
+    name = benches;
+    config = Criterion::default().with_measurement(CyclesPerByte);
+    targets = bench
+);
+
+criterion_main!(benches);
+
+*/
